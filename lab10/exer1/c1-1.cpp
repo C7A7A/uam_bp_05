d56847c8bfb261acbefd54fb701267e0f9b0cb4c
C7A7A <czajkamateusz999@mgail.com>
2021-12-17 13:03:05 +0100

    compiler options

---
+++ lab10/exer1/c1-1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	long abc[10];
+	long def[10];
+
+	for(int i=0;i<30;i++) abc[i]=i;
+	abc[0]++;
+	for(int i=0;i<10;i++) cout << def[i] << endl;
+	cout << sizeof(long) << sizeof(int) << sizeof(long long) << endl;	
+}
